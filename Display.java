1406ae822d549f959426bd23252cdb981222a82b
Mozif <bmozif@gmail.com>
2018-09-29 21:16:45 +0530

    Learnign merging conflicts

--- Display.java
+++ Display.java
@@ -9,6 +9,8 @@ public static void main(String args[])
     System.out.println("GithubSecondLine");
      System.out.println("GithubThirdLine");
      System.out.println("GithubFourthLine");
+     System.out.println("GithubFourthLine");
+     System.out.println("GithubFifthLine");
 
    
 }
