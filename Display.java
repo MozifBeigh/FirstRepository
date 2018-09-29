317cd58a1b9df2cc4d0cdde4fd5ebdbae0a6f5fe
MozifBeigh <mmozif03@gmail.com>
2018-09-29 21:44:31 +0530

    Update Display.java

--- Display.java
+++ Display.java
@@ -6,8 +6,6 @@ class Display{
 public static void main(String args[])
 {
     System.out.println("GithubfirstLine");
-    System.out.println("GithubSecondLine");
-     System.out.println("GithubThirdLine");
    
 }
 }
