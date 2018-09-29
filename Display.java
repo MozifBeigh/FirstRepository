c0f9f78138585f9c05d74653a4398d8b65b13df5
MozifBeigh <mmozif03@gmail.com>
2018-09-30 02:28:58 +0530

    Update Display.java

--- Display.java
+++ Display.java
@@ -5,10 +5,12 @@ import java.util.*;
 class Display{
 public static void main(String args[])
 {
-    System.out.println("GithubfirstLine");
+     System.out.println("GithubfirstLine");
 
-    System.out.println("GithubSecondLine");
+     System.out.println("GithubSecondLine");
      System.out.println("GithubThirdLine");
+    
+     just another statement
 
      System.out.println("GithubFourthLine");
      System.out.println("GithubFifthLine");
